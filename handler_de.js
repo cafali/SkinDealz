654479586ae17637ee6950ac8fba300fad80beb7
cafali <77307171+cafali@users.noreply.github.com>
2024-07-21 14:40:58 +0200

    Add files via upload

---
+++ handler_de.js
@@ -0,0 +1,56 @@
+function clickOnlineBankTransferButton() {
+    const bankTransferButton = document.querySelector('.adyen-checkout__payment-method--directEbanking button');
+    if (bankTransferButton) {
+        bankTransferButton.click();
+    } else {
+        console.error('Online Bank Transfer button not found');
+    }
+}
+
+function handleMutation(mutationsList, observer) {
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            if (document.querySelector('.adyen-checkout__payment-method--directEbanking')) {
+                observer.disconnect();
+                clickOnlineBankTransferButton();
+                break;
+            }
+        }
+    }
+}
+
+const observerOptions = { childList: true, subtree: true };
+const observer = new MutationObserver(handleMutation);
+observer.observe(document.body, observerOptions);
+
+function loadConfirmScript() {
+    const confirmScript = document.createElement('script');
+    confirmScript.src = chrome.runtime.getURL('confirm.js');
+    document.head.appendChild(confirmScript);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadConfirmScript();
+});
+
+function clickButtonWithDelay() {
+    const buttons = document.querySelectorAll('.adyen-checkout__button');
+    let targetButton;
+    buttons.forEach(button => {
+        if (button.textContent.trim() === "Continue to Online bank transfer.") {
+            targetButton = button;
+        }
+    });
+    if (targetButton) {
+        targetButton.click();
+    } else {
+        console.error("Button not found");
+    }
+}
+
+setTimeout(clickButtonWithDelay, 1500);
+
+
+setTimeout(function() {
+    window.location.href = 'https://skinport.com/account/orders';
+}, 5000);
