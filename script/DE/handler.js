9b5279528905936c3d2d68e702a2e70ce56b7336
cafali <77307171+cafali@users.noreply.github.com>
2025-01-17 15:45:17 +0100

    added handler.js random delay to reduce CAPTCHA detection during KLARNA payment selection

--- script/DE/handler.js
+++ script/DE/handler.js
@@ -1,31 +1,37 @@
-// Check for the Klarna button
+// random interval between 385-440 ms
+function getRandomInterval(min = 385, max = 440) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// check for the Klarna button
 const klarnaClickInterval = setInterval(() => {
-      // Locate button by text content
+    // locate by text content
     const klarnaButton = Array.from(document.querySelectorAll('button')).find(button =>
-        button.textContent.includes("Pay now with Klarna.")     // Locate button by text content
+        button.textContent.includes("Pay now with Klarna.") // Locate button by text content
     );
 
     if (klarnaButton) {
-        klarnaButton.click(); // Click the button if found
+        klarnaButton.click(); // click button if found
         console.log("Clicked on Klarna button.");
         clearInterval(klarnaClickInterval);
     }
-}, 400); // Check every X ms
+}, getRandomInterval()); // randomize check interval (ms)
 
-// Interval to repeatedly check for the "Continue to Pay now with Klarna." button
+// repeatedly check for "Continue to Pay now with Klarna." button
 const klarnaContinueClickInterval = setInterval(() => {
-    // Locate button by text content
+    // Locate button by text
     const klarnaContinueButton = Array.from(document.querySelectorAll('button')).find(button =>
         button.textContent.includes("Continue to Pay now with Klarna.")
     );
 
     if (klarnaContinueButton) {
-        klarnaContinueButton.click(); // Click the button if found
+        klarnaContinueButton.click(); // Click button if found
         console.log("Clicked on 'Continue to Pay now with Klarna' button.");
-        clearInterval(klarnaContinueClickInterval); // Stop the interval after clicking
+        clearInterval(klarnaContinueClickInterval); // Stop interval after clicking
     }
-}, 400); // Check every X ms
+}, getRandomInterval()); // Randomize check interval
 
+// Timeout to open the market page
 setTimeout(function() {
     window.open('https://skinport.com/market?sort=date&order=desc', '_self');
-}, 4500); //opens main site to market
+}, 4500); // redirect to LIVE  
